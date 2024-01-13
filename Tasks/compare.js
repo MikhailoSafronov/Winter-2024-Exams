21df507ed4458b1f4db7dd2213f9ca4798baadbd
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-13 23:15:51 +0200

    5-compare Step 5

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,19 +1,16 @@
-// Step 4: Improved code simplicity
-//Removed the unnecessary variable areEqual and simplified the return statement using every
-//changed let to const if possible
+// Step 5
+//Combined the conditions in the return statement for better quality and readability
 const compareDictionaries = (firstDictionary, ...otherDictionaries) => {
   const secondDictionary = otherDictionaries[0];
   const keysFirst = Object.keys(firstDictionary);
   const keysSecond = Object.keys(secondDictionary);
 
-  if (keysFirst.join('-') !== keysSecond.join('-')) {
-    return false;
-  }
-
-  return keysFirst.every(key => firstDictionary[key] === secondDictionary[key]);
+  return keysFirst.join('-') === keysSecond.join('-') &&
+      keysFirst.every(key => firstDictionary[key] === secondDictionary[key]);
 };
 
 module.exports = compareDictionaries;
+
 
 
 
