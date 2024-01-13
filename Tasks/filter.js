1cda4285c27cf0be89ac0af03acf110ee3e202a4
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 01:20:49 +0200

    2-filter Step 2

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,17 @@
 // Filter array by type name
-//Step 1
-//Renamed Function and Variables
-filterByType = (array, typeName) => {
-  remove = [];
-  for (element of array) {
-    index= array.indexOf(element);
+//Step 2
+// Add "use strict"
+//Add const and let
+'use strict'
+const filterByType = (array, typeName) => {
+  const remove = [];
+  for (const element of array) {
+    const index= array.indexOf(element);
     if (typeof array[index] !== typeName) {
       remove.unshift(index);
     }
   }
-  for (index of remove) array.splice(index, 1);
+  for (const index of remove) array.splice(index, 1);
   return array;
 };
 
