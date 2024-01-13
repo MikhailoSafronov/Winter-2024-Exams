45e0b9f58876932c7e3a69cb83504537f2b40bb4
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 01:50:24 +0200

    3-invert Step 3

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,16 +1,15 @@
 // Reverse an array, you can't use .reverse()
-//Step 2
-//Rename Function, parameters and variables
+//Step 3
+//rename the input array
+//Remove unused code
+//Remove hardcode
 'use strict';
 
-const reverseArray = (inputArray, i, j, k) => {
-  const reversedArray = Object.keys(inputArray, 4);
-  reversedArray.forEach((_, index) => {
-    reversedArray[index] = inputArray.pop();
-    ((unused) => {
-      return unused;
-    })(740);
-  }, 7);
+const reverseArray = (inputArray) => {
+  const reversedArray = [];
+  inputArray.forEach((_, index) => {
+    reversedArray.push(inputArray[inputArray.length - 1 - index]);
+  });
   return reversedArray;
 };
 
