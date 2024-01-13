5d802c96b5750f3bf0a35fa5159a11e0982eaa77
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-13 22:52:57 +0200

    2-compare Step2

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,17 +1,15 @@
 // Compare two dictionaries
 
-//Step 1
-//add 'use strict'
-//Renamed the function and arguments for better clarity
-'use scrict'
-const compareDictionaries = (firstValues, ...otherValues) => {
-  const secondValues = otherValues[0];
-  let keysFirst = Object.keys(firstValues);
-  let keysSecond = Object.keys(secondValues);
+//Step 2
+//renamed the variables
+const compareDictionaries = (firstDictionary, ...otherDictionaries) => {
+  const secondDictionary = otherDictionaries[0];
+  let keysFirst = Object.keys(firstDictionary);
+  let keysSecond = Object.keys(secondDictionary);
   if (keysFirst.join('-') !== keysSecond.join('-')) return false;
   let areEqual = true;
   for (const key of keysFirst) {
-    if (firstValues[key] === secondValues[key]) areEqual = areEqual && true;
+    if (firstDictionary[key] === secondDictionary[key]) areEqual = areEqual && true;
     else {
       areEqual = areEqual && false;
     }
