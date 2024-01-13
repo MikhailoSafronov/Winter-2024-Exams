8794e76cdb2ed15d9611fcb5e38fde24fe32edc9
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-13 23:22:56 +0200

    6-compare Step 6

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,16 +1,14 @@
-// Step 5
-//Combined the conditions in the return statement for better quality and readability
+//Step 6
+//Replaced the unnecessary variable secondDictionary directly in the object within the Object.keys(otherDictionaries[0]) call.
+// Replaced secondDictionary[key] with otherDictionaries[0][key] to obtain the value of the key without creating an additional variable.
 const compareDictionaries = (firstDictionary, ...otherDictionaries) => {
-  const secondDictionary = otherDictionaries[0];
   const keysFirst = Object.keys(firstDictionary);
-  const keysSecond = Object.keys(secondDictionary);
+  const keysSecond = Object.keys(otherDictionaries[0]);
 
   return keysFirst.join('-') === keysSecond.join('-') &&
-      keysFirst.every(key => firstDictionary[key] === secondDictionary[key]);
+      keysFirst.every(key => firstDictionary[key] === otherDictionaries[0][key]);
 };
-
 module.exports = compareDictionaries;
-
 
 
 
