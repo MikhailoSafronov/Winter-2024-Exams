fbd87ceb4496619cdbb87cc8e59ab687c8901419
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 00:31:18 +0200

    1-day Step 1

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,17 @@
 // Get day number
+//Step 1
+//Renamed function, array and variables
+//Use const and let
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+const parseDay = (inputString) => {
+  let index;
+  for (index = 0; index < daysOfWeek.length; index++) {
+    if (inputString.startsWith(daysOfWeek[index].toLowerCase())) {
+      return index + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
