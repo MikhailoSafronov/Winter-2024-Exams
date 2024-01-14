399ee212966adb94183a43467ae95cbef9d37f9e
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 22:11:55 +0200

    4-only Step 4

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,18 +1,19 @@
 // Copy only listed values from dict
-//Step 3
-//Remove the unused code blocks and parameters
-'use strict'
-const filterObjectKeys = (inputObject, ...keysToKeep) => {
+//Step 4
+//Instead of deleting keys directly from the input object,  created a new object (filteredObject) to store the filtered keys
+// Used Object.keys(inputObject) to get the keys of the input object directly in the loop to improve readability.
+'use strict';
 
-  const objectKeys = Object.keys(inputObject);
-  objectKeys.forEach((key) => {
+const filterObjectKeys = (inputObject, ...keysToKeep) => {
+  const filteredObject = {};
+
+  Object.keys(inputObject).forEach((key) => {
     if (keysToKeep.includes(key)) {
-    } else {
-      delete inputObject[key];
+      filteredObject[key] = inputObject[key];
     }
   });
 
-  return inputObject;
+  return filteredObject;
 };
 
 module.exports = filterObjectKeys;
