ebea465887651bf753d23bffa1e6faeb97d182ad
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 22:23:29 +0200

    2-split Step 2

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,13 +1,12 @@
 // Split an array into two parts
-//Step 1
-//Add 'use strict'
-//Add const
+//Step 2
+//Rename the function, parameters and variables
 'use strict'
-let split_Array = (index, array) => {
-  const begin = array.slice(0, index);
-  const len = array.length;
-  array = array.slice(index, len);
-  return [begin, array];
+let splitArrayAtIndex = (index, inputArray) => {
+  const firstPart = inputArray.slice(0, index);
+  const arrayLength = inputArray.length;
+  const secondPart = inputArray.slice(index, arrayLength);
+  return [firstPart, secondPart];
 };
 
-module.exports = split_Array;
+module.exports = splitArrayAtIndex;
