55599d55669c9a19fcb5b27f193a60fee12614f1
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 22:20:48 +0200

    1-split Step 1

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,7 +1,10 @@
 // Split an array into two parts
-
+//Step 1
+//Add 'use strict'
+//Add const
+'use strict'
 let split_Array = (index, array) => {
-  begin = array.slice(0, index);
+  const begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
