305023f2598ecafc3e18718c24db5816f4eb08ae
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 02:46:35 +0200

    3-month Step 3

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,28 +1,23 @@
 // Get month number
-//Step 2
-//Rename function, parameters and variables
-'use strict'
+//Step 3
+//rename i to monthIndex for better readability
+// Removed unnecessary variable l and directly used MONTHS.length in the loop condition
+//Combined the declaration and initialization of lowercaseInput
+'use strict';
+
 const MONTHS = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+  'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
 ];
 
 const getMonthIndex = (input) => {
-  const lowercaseInput = MONTHS.length;
-  for (let i = 0; i < lowercaseInput; i++) {
-    if (input.toLowerCase().startsWith(MONTHS[i])) return i + 1;
+  const lowercaseInput = input.toLowerCase();
+  for (let monthIndex = 0; monthIndex < MONTHS.length; monthIndex++) {
+    if (lowercaseInput.startsWith(MONTHS[monthIndex])) {
+      return monthIndex + 1;
+    }
   }
   return -1;
 };
 
 module.exports = getMonthIndex;
+
