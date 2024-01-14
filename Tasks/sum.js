a5c3638c73e33defb6e58355e0a0f0c57ada88a4
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 23:44:46 +0200

    3-sum Step 3

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,23 +1,15 @@
 // Sum all numbers from an array
-//Step 2
-//Renamed the function, variables and parameters
+//Step 3
+//Removed the unused and unnecessary code blocks
 'use strict';
-
-const calculateSum = function (numbersArray = () => {}) {
+const calculateSum = function (numbersArray =[]) {
   let sumArray = [0];
-  let constantValue = 5;
-
   for (let num of numbersArray) {
-    let dataType = typeof num;
-
-    if (dataType === 'number') {
-      if (sumArray.length > 0) {
+    if (typeof num === 'number') {
         const newSum = sumArray[sumArray.length - 1] + num;
         sumArray.push(newSum);
-      }
     }
   }
-
   return sumArray[sumArray.length - 1];
 };
 
