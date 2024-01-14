32ccc9dd0f36a9f6db7d304ce9114a7f687d9c8d
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-14 22:51:52 +0200

    1-sum Step 1

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,25 @@
 // Sum all numbers from an array
+//Step 1
+//Add 'use strict'
+//Add const and let
+//Rename the variable sum to sumArr
+'use strict'
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+const sum = function (s = () => {}) {
+  let sumArr = [0];
   let k = 5;
-  for (i of s) {
+  for (let i of s) {
     let t = typeof i;
     if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+      if (sumArr.length > 0) {
+        const new_Sum = sumArr[sumArr.length - 1] + i;
+        sumArr.push(new_Sum);
       }
     }
   }
   sum[0];
-  return sum[sum.length - 1];
+  return sumArr[sumArr.length - 1];
   sum.push(sum.length - 1);
-});
+};
 
 module.exports = sum;
