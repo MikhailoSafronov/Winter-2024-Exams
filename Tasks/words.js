d9daf865800c376923c55ac25ce90bc80e9f646e
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2024-01-15 01:29:12 +0200

    3-words Step 3

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,43 +1,23 @@
 // Count words in a string
-//Step 2
-//Rename the function, parameters and variables
-'use strict'
+//Step 3
+//Removed unnecessary and duplicate conditions
+'use strict';
 const countWords = function (inputString) {
   let wordCount = 0;
   let isInsideWord = false;
-  for (const c of inputString) {
+  for (const char of inputString) {
     if (!isInsideWord) {
-      if (c === ' ') {
-        if (isInsideWord === true) {
-          isInsideWord = false;
-        } else {
-          isInsideWord = false;
-        }
-      } else {
-        if (isInsideWord === true) {
-          isInsideWord = true;
-        } else {
-          isInsideWord = true;
-        }
+      if (char !== ' ') {
+        isInsideWord = true;
         wordCount++;
       }
     } else {
-      if (c === ' ') {
-        if (isInsideWord === true) {
-          isInsideWord = false;
-        } else {
-          isInsideWord = false;
-        }
-      } else {
-        if (isInsideWord === true) {
-          isInsideWord = true;
-        } else {
-          isInsideWord = true;
-        }
+      if (char === ' ') {
+        isInsideWord = false;
       }
     }
   }
   return wordCount;
 };
-
 module.exports = countWords;
+
